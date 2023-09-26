045ab185e7f71f79103cb858ab0e3982b20cff33
mtrzuskowski <mich.trzus@gmail.com>
2023-09-26 14:03:39 -0400

    implemented pictures service

--- backend/routes.py
+++ backend/routes.py
@@ -35,7 +35,9 @@ def count():
 ######################################################################
 @app.route("/picture", methods=["GET"])
 def get_pictures():
-    pass
+    if data:
+        return jsonify(data), 200
+    return {"message": "Internal server error"}, 500
 
 ######################################################################
 # GET A PICTURE
@@ -44,7 +46,10 @@ def get_pictures():
 
 @app.route("/picture/<int:id>", methods=["GET"])
 def get_picture_by_id(id):
-    pass
+    for picture in data:
+        if picture.get("id") == id:
+            return picture
+    return {"message": "Internal server error"}, 404
 
 
 ######################################################################
@@ -52,7 +57,16 @@ def get_picture_by_id(id):
 ######################################################################
 @app.route("/picture", methods=["POST"])
 def create_picture():
-    pass
+    picture_new = request.json
+
+    for picture in data:
+        if picture_new["id"] == picture["id"]:
+            return {
+                "Message": f"picture with id {picture_new['id']} already present"
+            }, 302
+
+    data.append(picture_new)
+    return picture_new, 201
 
 ######################################################################
 # UPDATE A PICTURE
@@ -61,11 +75,21 @@ def create_picture():
 
 @app.route("/picture/<int:id>", methods=["PUT"])
 def update_picture(id):
-    pass
+    picture_new = request.json
+    for index, picture in enumerate(data):
+        if picture.get("id") == id:
+             data[index] = picture_new
+             return picture, 200
+    return {"message": "picture not found"}, 404
 
 ######################################################################
 # DELETE A PICTURE
 ######################################################################
 @app.route("/picture/<int:id>", methods=["DELETE"])
 def delete_picture(id):
-    pass
+    for picture in data:
+        if picture["id"] == id:
+            data.remove(picture)
+            return "", 204
+
+    return {"message": "picture not found"}, 404
